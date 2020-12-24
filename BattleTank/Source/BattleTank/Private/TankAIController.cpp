681ee1c1a78b10bda2089d03312ac0daec6d0b3a
Dasith98 <dasithrathnasinghe@gmail.com>
2020-12-24 08:35:56 +0530

    created TankPlayerController and TankAIController and used them as parents of TankPlayerController_BP and TankAIController_BP

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankAIController.h"
+
+ATank* ATankAIController::GetControlledTank() const {
+	return Cast<ATank>(GetPawn());
+};
+
+void ATankAIController::BeginPlay() {
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp,Warning,TEXT("AITankController is not possesing"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s AITankController is possesing"),*(ControlledTank->GetName()));
+	}
+}
--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -2,6 +2,22 @@
 
 #include "TankPlayerController.h"
 
+
+
 ATank* ATankPlayerController::GetControlledTank() const {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank =GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController is possesing %s"), *(ControlledTank->GetName()))
+	}
+	
 }
